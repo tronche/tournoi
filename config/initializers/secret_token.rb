fcd93b20ff0f4dbea78eee3a444e3c31884c6d75
domi <domikovitch@yahoo.fr>
2011-04-05 19:29:01 +0200

    Commit initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tournoi::Application.config.secret_token = '867867e0642053ece067a4ff9aed774f1864f144a541a7de1dbed44c152e96222ff6dcb75bde8200f7c58362dcb339dcaf45003cf023aa6b9b573bcdd431a7ee'
